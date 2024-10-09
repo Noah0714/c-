59d6c1f0e779ff95aa269f1c1cd478904d86f054
Noah0714 <3142137215@qq.com>
2024-10-09 19:49:01 +0800

    Add files via upload

---
+++ sushu.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/*int main()
+{
+    int i,n,sum;
+    scanf("%d",&i);
+    for(n=2;n<=i;n++)
+    {
+        if(i%n==0)
+        break;  
+    }
+    if(i==n)
+    printf("i为素数\n");
+    else
+    printf("i为非素数\n");
+    return 0;
+}*/
+/*int main()
+{
+    int n,i,sum;
+    sum=0;
+    scanf("%d",&n);
+    for(i=2;i<=n;i++)
+    {
+        if(n%i!=0)
+        sum=sum+1;
+        else
+        break;
+
+    }
+    if(sum==(n-2))
+    printf("n is su");
+    else
+    printf("No");
+
+}*/
